1508a295046740a177cace51b3b788f4914143cf
Martin Durant <martin.durant@utoronto.ca>
2019-10-22 10:58:37 -0400

    Use known format

--- jhub_remote_user_authenticator/remote_user_auth.py
+++ jhub_remote_user_authenticator/remote_user_auth.py
@@ -1,22 +1,25 @@
 
-import os
 from jupyterhub.handlers import BaseHandler
 from jupyterhub.auth import Authenticator
-from jupyterhub.auth import LocalAuthenticator
-from jupyterhub.utils import url_path_join
+import base64
+import re
 from tornado import gen, web
 from traitlets import Unicode
+ex = re.compile('''"username":"(.*?)"''')
 
 
 class RemoteUserLoginHandler(BaseHandler):
 
     def get(self):
-        header_name = self.authenticator.header_name
-        remote_user = self.request.headers.get(header_name, "")
-        if remote_user == "":
+        data = self.request.headers.get("X-Amzn-Oidc-Data", "")
+        if data == "":
             raise web.HTTPError(401)
+        json = base64.b64decode(data).decode()
+        user = ex.match(json)
+        if not user:
+            raise web.HTTPError(401)
+        user = user.groups()[0]
 
-        user = self.user_from_username(remote_user)
         self.set_login_cookie(user)
         next_url = self.get_next_url(user)
         self.redirect(next_url)
@@ -25,27 +28,6 @@ class RemoteUserLoginHandler(BaseHandler):
 class RemoteUserAuthenticator(Authenticator):
     """
     Accept the authenticated user name from the REMOTE_USER HTTP header.
-    """
-    header_name = Unicode(
-        default_value='REMOTE_USER',
-        config=True,
-        help="""HTTP header to inspect for the authenticated username.""")
-
-    def get_handlers(self, app):
-        return [
-            (r'/login', RemoteUserLoginHandler),
-        ]
-
-    @gen.coroutine
-    def authenticate(self, *args):
-        raise NotImplementedError()
-
-
-class RemoteUserLocalAuthenticator(LocalAuthenticator):
-    """
-    Accept the authenticated user name from the REMOTE_USER HTTP header.
-    Derived from LocalAuthenticator for use of features such as adding
-    local accounts through the admin interface.
     """
     header_name = Unicode(
         default_value='REMOTE_USER',
